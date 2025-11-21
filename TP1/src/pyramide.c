af8762e818274b47d8762e5a58faefa2262a7352
peroumalsriraam-coder <peroumalsriraam@gmail.com>
2025-11-21 15:26:17 +0100

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,31 @@
 
+#include <stdio.h>
+
+int main() {
+    int n = 5;  // Hauteur de la pyramide
+    int i, j;
+
+    for(i = 1; i <= n; i++) {
+        // Affichage des espaces pour centrer la pyramide
+        for(j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+
+        // Affichage des nombres croissants
+        for(j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        // Affichage des nombres décroissants
+        for(j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        // Passage à la ligne suivante
+        printf("\n");
+    }
+
+    printf("Génération de la pyramide terminée.\n");
+
+    return 0;
+}
